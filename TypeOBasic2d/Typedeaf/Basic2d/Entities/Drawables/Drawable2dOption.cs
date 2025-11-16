c448a58829d1ae1f6b26cf87c3c9f99ac41e2f2f
Simon Blixt Haggren <simonhaggren@gmail.com>
2025-11-16 07:30:11 +0100

    Added comments

--- TypeOBasic2d/Typedeaf/Basic2d/Entities/Drawables/Drawable2dOption.cs
+++ TypeOBasic2d/Typedeaf/Basic2d/Entities/Drawables/Drawable2dOption.cs
@@ -5,10 +5,25 @@ namespace TypeOEngine.Typedeaf.TypeOBasic2d
 {
     namespace Entities.Drawables
     {
+        /// <summary>
+        /// Represents an option for configuring a 2D drawable object.
+        /// </summary>
+        /// <remarks>This class provides configuration options specific to 2D drawable objects, such as
+        /// their position.</remarks>
+        /// <typeparam name="D">The type of the drawable object, which must derive from <see cref="Drawable2d"/>.</typeparam>
         public class Drawable2dOption<D> : DrawableOption<D> where D : Drawable2d
         {
+            /// <summary>
+            /// Gets or sets the position of the object in 2D space.
+            /// </summary>
             public Vec2? Position { get; set; }
 
+            /// <summary>
+            /// Assigns the specified position to the object and returns a value indicating whether the operation was
+            /// successful.
+            /// </summary>
+            /// <param name="obj">The object to which the position will be assigned.</param>
+            /// <returns><see langword="true"/> if the operation was successful; otherwise, <see langword="false"/>.</returns>
             public override bool Create(D obj)
             {
                 if(Position.HasValue)
--- TypeOBasic2d/Typedeaf/Basic2d/Entities/Drawables/DrawableTextureOption.cs
+++ TypeOBasic2d/Typedeaf/Basic2d/Entities/Drawables/DrawableTextureOption.cs
@@ -4,10 +4,27 @@ namespace TypeOEngine.Typedeaf.TypeOBasic2d
 {
     namespace Entities.Drawables
     {
+        /// <summary>
+        /// Represents an option for configuring a <see cref="DrawableTexture"/> object, including its associated texture.
+        /// </summary>
+        /// <remarks>This class provides a way to specify a <see cref="Texture"/> that can be applied to
+        /// a <see cref="DrawableTexture"/> during its creation. It extends the functionality of
+        /// <see cref="Drawable2dOption{T}"/> to include texture-specific configuration.</remarks>
         public class DrawableTextureOption : Drawable2dOption<DrawableTexture>
         {
+            /// <summary>
+            /// Gets or sets the texture associated with the object.
+            /// </summary>
             public Texture Texture { get; set; }
 
+            /// <summary>
+            /// Creates and initializes a <see cref="DrawableTexture"/> object.
+            /// </summary>
+            /// <remarks>If the <see cref="Texture"/> property is not <see langword="null"/>, it will
+            /// be assigned to the <paramref name="obj"/> instance.</remarks>
+            /// <param name="obj">The <see cref="DrawableTexture"/> instance to be initialized.</param>
+            /// <returns><see langword="true"/> if the object was successfully created and initialized; otherwise,
+            /// <see langword="false"/>.</returns>
             public override bool Create(DrawableTexture obj)
             {
                 if(!base.Create(obj)) return false;
