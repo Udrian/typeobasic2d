5dfff1b64bec0be6e36f7db536b1d28236828227
Udrian <simonhaggren@gmail.com>
2026-05-23 17:43:50 +0200

    Added Debug2DDrawable that highlight components

--- TypeDBasic2d/TypeDBasic2dInitializer.cs
+++ TypeDBasic2d/TypeDBasic2dInitializer.cs
@@ -1,7 +1,14 @@
-﻿using TypeD;
+﻿using System.Collections.Generic;
+using System.Linq;
+using TypeD;
 using TypeD.Models.Data;
+using TypeD.Models.Data.Hooks;
+using TypeD.Models.Interfaces;
 using TypeD.Models.Providers.Interfaces;
 using TypeDBasic2d.Components;
+using TypeDBasic2d.Typedeaf.Core.Entities.Drawables;
+using TypeDCore.Models.Data.Hooks;
+using TypeOEngine.Typedeaf.Core.Common;
 
 namespace TypeDBasic2d
 {
@@ -10,21 +17,86 @@ namespace TypeDBasic2d
         // Providers
         IComponentProvider ComponentProvider { get; set; }
 
+        // Models
+        IHookModel HookModel { get; set; }
+
+        // Data
+        Dictionary<string, Debug2DDrawable> DebugDrawablesMap { get; set; }
+        Debug2DDrawable CurrentFocusedDebug2DDrawable { get; set; }
+
         public override void Initializer(Project project)
         {
             // Providers
             ComponentProvider = Resources.Get<IComponentProvider>();
 
+            // Models
+            HookModel = Resources.Get<IHookModel>();
+
             // Data
-            ComponentProvider.AddBaseTypeComponent(Basic2dComponent.Drawable2dComponent());
+            DebugDrawablesMap = new Dictionary<string, Debug2DDrawable>();
+
             ComponentProvider.AddBaseTypeComponent(Basic2dComponent.Entity2dComponent());
+            ComponentProvider.AddBaseTypeComponent(Basic2dComponent.Drawable2dComponent());
+            //ComponentProvider.AddBaseTypeComponent(Basic2dComponent.)
+
+            // Hooks
+            HookModel.AddHook<TypeOObjectAddedToViewHook>(AddDebugDrawableComponent);
+            HookModel.AddHook<PropertyChangedHook>(ComponentPropertyChanged);
+            HookModel.AddHook<ComponentFocusHook>(ComponentFocusChanged);
         }
 
         public override void Uninitializer()
         {
             // Data
-            ComponentProvider.RemoveBaseTypeComponent(Basic2dComponent.Drawable2dComponent());
             ComponentProvider.RemoveBaseTypeComponent(Basic2dComponent.Entity2dComponent());
+            ComponentProvider.RemoveBaseTypeComponent(Basic2dComponent.Drawable2dComponent());
+
+            // Hooks
+            HookModel.RemoveHook<TypeOObjectAddedToViewHook>(AddDebugDrawableComponent);
+            HookModel.RemoveHook<PropertyChangedHook>(ComponentPropertyChanged);
+            HookModel.RemoveHook<ComponentFocusHook>(ComponentFocusChanged);
+        }
+
+        internal void AddDebugDrawableComponent(TypeOObjectAddedToViewHook hook)
+        {
+            Vec2 position = hook.Component.Properties.FirstOrDefault(p => p.Name == "Position")?.ToValue<Vec2>() ?? Vec2.Zero;
+            Vec2 size = hook.Component.Properties.FirstOrDefault(p => p.Name == "Size")?.ToValue<Vec2>() ?? Vec2.Zero;
+            var drawable = hook.Context.Game.Scenes.CurrentScene.Drawables.Create<Debug2DDrawable>();
+            drawable.TypeOObject = hook.TypeOObject;
+            drawable.Position = position;
+            drawable.Size = size;
+            drawable.UpdateLines();
+            DebugDrawablesMap.Add(hook.Component.ID, drawable);
+        }
+
+        internal void ComponentPropertyChanged(PropertyChangedHook hook)
+        {
+            if (DebugDrawablesMap.ContainsKey(hook.ID))
+            {
+                var drawable = DebugDrawablesMap[hook.ID];
+                if (hook.Property.Name == "Position" || hook.Property.Name == "Size")
+                {
+                    Vec2 position = hook.Property.Name == "Position" ? hook.Property.ToValue<Vec2>() : drawable.Position;
+                    Vec2 size = hook.Property.Name == "Size" ? hook.Property.ToValue<Vec2>() : drawable.Size;
+                    drawable.Position = position;
+                    drawable.Size = size;
+                    drawable.UpdateLines();
+                }
+            }
+        }
+
+        internal void ComponentFocusChanged(ComponentFocusHook hook)
+        {
+            if (CurrentFocusedDebug2DDrawable != null)
+            {
+                CurrentFocusedDebug2DDrawable.Focused = false;
+                CurrentFocusedDebug2DDrawable = null;
+            }
+            if (DebugDrawablesMap.ContainsKey(hook.Component.ID))
+            {
+                CurrentFocusedDebug2DDrawable = DebugDrawablesMap[hook.Component.ID];
+                CurrentFocusedDebug2DDrawable.Focused = true;
+            }
         }
     }
 }
---
+++ TypeDBasic2d/Typedeaf/Core/Entities/Drawables/Debug2DDrawable.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using TypeOEngine.Typedeaf.Basic2d.Engine.Graphics;
+using TypeOEngine.Typedeaf.Core.Common;
+using TypeOEngine.Typedeaf.Core.Engine;
+using TypeOEngine.Typedeaf.Core.Engine.Graphics.Interfaces;
+using TypeOEngine.Typedeaf.Desktop.Engine.Services;
+using TypeOEngine.Typedeaf.TypeOBasic2d.Entities;
+using TypeOEngine.Typedeaf.TypeOBasic2d.Entities.Drawables;
+
+namespace TypeDBasic2d.Typedeaf.Core.Entities.Drawables
+{
+    public class Debug2DDrawable : Drawable2d
+    {
+        public override Vec2 Size { get; set; }
+        private List<Vec2> Lines { get; set; }
+
+        public bool Focused { get; set; }
+        public MouseInputService MouseInputService { get; set; }
+        public TypeOObject TypeOObject { get; set; }
+
+        public override void Draw(ICanvas canvas)
+        {
+            var mousePos = MouseInputService.MousePosition;
+            Color drawColor = Focused ? Color.Green : Color.Red;
+
+            var pos = GetPos();
+
+            if (mousePos.X >= pos.X && mousePos.X <= (pos.X + Size.X) && mousePos.Y >= pos.Y && mousePos.Y <= (pos.Y + Size.Y))
+                drawColor = Color.MarineBlue;
+
+            UpdateLines();
+            canvas.DrawLines(Lines, drawColor);
+        }
+
+        protected override void Cleanup()
+        {
+        }
+
+        protected override void Initialize()
+        {
+            DrawOrder = int.MaxValue;
+            Lines = new List<Vec2>() { new Vec2(), new Vec2(), new Vec2(), new Vec2(), new Vec2() };
+            Focused = false;
+            UpdateLines();
+        }
+
+        public void UpdateLines()
+        {
+            var pos = GetPos();
+
+            Lines[0] = new Vec2(0, 0) + pos;
+            Lines[1] = new Vec2(Size.X, 0) + pos;
+            Lines[2] = new Vec2(Size.X, Size.Y) + pos;
+            Lines[3] = new Vec2(0, Size.Y) + pos;
+            Lines[4] = new Vec2(0, 0) + pos;
+        }
+
+        private Vec2 GetPos()
+        {
+
+            if (TypeOObject is Entity2d)
+            {
+                return new Vec2(((Entity2d)TypeOObject).ScreenBounds.Pos.X, ((Entity2d)TypeOObject).ScreenBounds.Pos.Y);
+            }
+            else if(TypeOObject is Drawable2d)
+            {
+                var pos = new Vec2(Position.X, Position.Y);
+                pos.X = pos.X + (((Drawable2d)TypeOObject).Entity as Entity2d)?.ScreenBounds.Pos.X ?? 0;
+                pos.Y = pos.Y + (((Drawable2d)TypeOObject).Entity as Entity2d)?.ScreenBounds.Pos.Y ?? 0;
+                return pos;
+            }
+
+            return Position;
+        }
+    }
+}
